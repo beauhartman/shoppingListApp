cb6534dd1b33494f81fc37d9cc1e0432f5ecd872
Beau <beauhartman@thinkful.com>
2014-11-12 17:02:40 -0600

    11.12.14 Changes git status

--- js/app.js
+++ js/app.js
@@ -23,7 +23,15 @@ $(document).ready(function() {
 	$()
 
 	//* User Wants to Mark Item as "Got It"
-	$()
+	$("#needgot").on("click", function() {
+		var el = $(this);
+		if (el.text() == el.data("text-swap")) {
+			el.text(el.data("text-original"));
+		} else {
+			el.data("text-original", el.text());
+			el.text(el.data("text-swap"));
+		}
+	});
 
 	//* User Wants to Start a New List
 	$()
