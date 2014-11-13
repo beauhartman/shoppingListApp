df72bc175a513d548ca8a208834a01f4c86ccaba
Beau <beauhartman@thinkful.com>
2014-11-13 17:13:37 -0600

    11.13.14 5pm

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
 $(document).ready(function() {
-	var deleteX = '<span class="delete"></span>'
-	var redGreen = '<span class="red green"></span>'
+
+	//* User wants to enter an item
+	//* user types in new item name
+	$("#add-new-item").keydown(function(enter) {
+		if (enter.keyCode == 13) {
+			$("#add-new-item").text();
+		}
+	})
+
+
+	//* user hits enter
+	//* new item name stored
+
+	//* Displaying the newly entered item as a list item
+
+	//* User wants to record they do(n't) have the item
+	$('#list').on('click', 'li', function() {
+		$(this).toggleClass('green');
+	})
+
+	//* User wants to reset the list
+	$(document).on("click", ".start-a-new-list", function() {
+		$("#list").remove("li");
+	})
 
 	//* User wants to delete an item from the list
 	$(document).on("click", ".delete", function() {
 		$(this).closest("li").remove();
 	})
-
-	//* User wants to reset the list
-	$(document).on("click", ".start-a-new-list", function() {
-		$("#list").empty();
-	})
-
-	//* User wants to record they have/do not have the item
-	$(document).on("click", "#haveit", function() {
-		if ($(this).hasClass("red")) {
-			$(this).removeClass("red");
-			$(this).addClass("green");
-		} else {
-			$(this).removeClass("green");
-			$(this).addClass("red");
-		}
-	})
-
-	//* User wants to add an item to the list
-	$(document).on("click", "#add-button", function() {
-		var getItem = $("#add-new-item").val();
-		$("#list > ul").append('<li>' + deleteX + getItem + redGreen + '</li>');
-		$("#add-new-item").val("");
-	})
-});
+})
