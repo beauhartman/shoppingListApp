558bbd2c1d76ebf06c12c9fea26c6199eb661aae
Beau <beauhartman@thinkful.com>
2014-11-13 12:00:19 -0600

    11.13.14 changes

--- js/app.js
+++ js/app.js
@@ -1,44 +1,25 @@
 $(document).ready(function() {
-	var deleteItem = '<span class="delete"></span>'
-	var button = '<button id="needgot" class="need" data-text-swap="Got It!"></button>'
-
-	//* User Types in New Item
-	$("#add-new-items").keydown(function(e) {
-		if (e.keyCode == 13) {
-			addItem();
-		}
+	//* User wants to delete an item from the list
+	$(document).on("click", ".delete", function() {
+		$(this).closest("li").remove();
 	})
 
-	function addItem() {
-		//* add new <p> with proper content
-		var item = $("#add-new-items").val();
-		var display = "<li> + deleteItem + item + button + </li>"
-		$("#list").prepend(display);
-		$("#add-new-items").val("");
-		$("#list").first("li").
-	}
-	
-	//* User Clicks "+" Button
-	$(".add-function").on("click", function() {
-		addItem();
+	//* User wants to reset the list
+	$(document).on("click", ".start-a-new-list", function() {
+		$("#list").empty();
 	})
 
-	//* User Wants to Remove List Item
-	$()
-
-	//* User Wants to Mark Item as "Got It"
-	$("#needgot").on("click", function() {
-		var needgot = $(this);
-		if (needgot.text() == needgot.data("text-swap")) {
-			needgot.text(needgot.data("text-original"));
+	//* User wants to record they have/do not have the item
+	$(document).on("click", "#haveit", function() {
+		if ($(this).hasClass("red")) {
+			$(this).removeClass("red");
+			$(this).addClass("green");
 		} else {
-			needgot.data("text-original", needgot.text());
-			needgot.text(needgot.data("text-swap"));
+			$(this).removeClass("green");
+			$(this).addClass("red");
 		}
-	});
+	})
 
-	//* User Wants to Start a New List
-	//* push ".start-a-new-list" button
-	//* reset "#list" section
-	$()
+	//* User wants to add an item to the list
+	
 });
